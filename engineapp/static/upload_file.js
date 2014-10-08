0e69f05ca82e1e9b31cfe3e0596a04c64bc442e5
Jerry Jiang <jerryjiang1128@gmail.com>
2014-10-07 21:16:15 -0400

    Implemented jQuery form pluging instead of normal jQuery.

--- engineapp/static/upload_file.js
+++ engineapp/static/upload_file.js
@@ -6,30 +6,31 @@ function handleSubmit() {
     var button = document.getElementById( "submit-button" );
     button.innerHTML = "Uploading...";
     button.disabled = true;
-
     // Hide the result callout panel
     document.getElementById( "file-link" ).style.display = "none";
-	
-	uploadFile();
+    return true;
 }
 
 // Displays the error message in the callout panel
-function displayError( message ) {
+function displayMessage( message, isError ) {
     var downloadMessage = document.getElementById( "file-link" );
     downloadMessage.style.display = "block";
     downloadMessage.innerHTML = message;
-    downloadMessage.classList.toggle( "error", true );
-    return false;
+    downloadMessage.classList.toggle( "error", isError );
+    var button = document.getElementById( "submit-button" );
+    button.innerHTML = "Upload";
+    button.disabled = false;
+    return !isError;
 }
 
 function validateFile() {
     // Check if there's no file.
     if($('#fileupload').val() == '') {
-        return displayError( "No file." );
+        return displayMessage( "No file.", true );
     }
     // Check if the file is too big.
     if ( !checkFileSize() ) {
-        return displayError( "Upload must be smaller than 16MB." );
+        return displayMessage( "Upload must be smaller than 16MB.", true );
     }
 
     return true;
@@ -41,38 +42,6 @@ function checkFileSize() {
         return false;
     }
     return true;
-}
-
-function uploadFile() {
-    $.ajax( {
-        url: $('#blobstore-url').data("url"),
-        type: "POST",
-        data: new FormData( $("form[name='fileupload-form']").get(0) ),
-        processData: false,
-        contentType: false,
-        success: function( data ) {
-            var downloadMessage = document.getElementById( "file-link" );
-            downloadMessage.classList.toggle( "error", false );
-            downloadMessage.innerHTML = data;
-        },
-        error: function( jqXHR, textStatus, errorThrown ) {
-            if ( errorThrown == "Bad Request" ) {
-                return displayError( jqXHR.responseText );
-            } else if( errorThrown == "Not Found" ) {
-                return displayError( "Error uploading file. Please refresh the page." );
-            } else {
-                return displayError( "An internal error has occurred! Please contact the creator of this site." );
-            }
-        },
-        complete: function( data ) {
-            var downloadMessage = document.getElementById( "file-link" );
-            downloadMessage.style.display = "block";
-            var button = document.getElementById( "submit-button" );
-            button.innerHTML = "Upload";
-            button.disabled = false;
-            $('#fileupload').val(null);
-        }
-    } );
 }
 
 $('#fileupload').on( 'dragenter', function(event) {
@@ -93,7 +62,27 @@ $('#fileupload').on( 'drop', function(event) {
     }
 });
 
+var options = {
+    beforeSubmit: handleSubmit,
+    resetForm: true,
+    error: function( jqXHR, textStatus, errorThrown ) {
+        if ( errorThrown == "Bad Request" ) {
+            return displayMessage( jqXHR.responseText, true );
+        } else if( errorThrown == "Not Found" ) {
+            return displayMessage( "Error uploading file. Please refresh the page.", true );
+        } else {
+            return displayMessage( "An internal error has occurred! Please contact the creator of this site.", true );
+        }
+    },
+    success: function( data ) {
+        displayMessage( data, false );
+    },
+};
+
+$('#fileupload-form').ajaxForm(options);
+
 $('#submit-button').click( function(evt) {
     evt.preventDefault();
-    handleSubmit();
+    $('#fileupload-form').ajaxSubmit(options);
+    return false;
 });
